c78790b7ff88857b7788fd8fa709f74244f0a028
jcheavy <jcheavy@hotmail.com>
2022-02-09 23:37:22 -0300

    Primeiro commit

---
+++ XadresDois/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using tabuleiro;
+namespace XadresDois
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p = new Posicao(4, 4);
+            Console.WriteLine("Linha :"+ p.Linha);
+            Console.WriteLine("Coluna :" + p.Coluna);
+        }
+    }
+}
